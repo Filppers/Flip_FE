3b5a15daf76eaa1b7c2f3d08a44df6276a2417d1
arky02 <kyean07@naver.com>
2024-10-17 05:13:55 +0900

    design: 버튼 style 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,8 +10,10 @@ const Button = ({
   return (
     <button
       className={`${
-        isSelected ? "bg-[#ff6f3f28] text-[#EB5A2A] font-[700]" : "bg-[#F3F4F6]"
-      }  text-[15px] w-fit min-w-[38px] font-medium rounded-full py-[5px] px-[10px] whitespace-nowrap  shadow-sm  hover:bg-[#ff6f3f28] hover:text-[#EB5A2A] hover:font-bold`}
+        isSelected
+          ? "bg-[#ff6f3f28] text-[#EB5A2A] font-semibold"
+          : "bg-[#F3F4F6] font-medium"
+      }  text-[14px] w-fit min-w-[38px] font-medium rounded-full py-[5px] px-[10px] whitespace-nowrap  shadow-sm  hover:bg-[#ff6f3f28] hover:text-[#EB5A2A] hover:font-bold`}
       onClick={onClick}
     >
       {text}
